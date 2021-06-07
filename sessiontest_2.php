2ff4c572f5e5e6962344ee8d8cd93022eb1e59e3
redrabbit1104 <redcaprabbit1104@gmail.com>
2021-06-08 07:11:22 +0900

    sessiontest_2

---
+++ sessiontest_2.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+<?php
+
+$_SESSION['date'] = date('c');
+
+echo $_SESSION['date'];
+echo '<br>';
+
+if(isset($_COOKIE['PHPSESSID'])){
+  setcookie('PHPSESSID', '', time() - 3600, '/');
+}
+
+session_destroy();
+
+// setcookie("id", '', time() -1800, '/');
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($_COOKIE);
+echo '</pre>'
+?>
+
+</body>
+</html>
